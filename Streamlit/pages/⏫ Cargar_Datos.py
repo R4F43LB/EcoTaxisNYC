293fcab6a048f84aa8f9d2624b229c23113c009b
LDavidRojasD <ldavidrd@gmail.com>
2024-04-21 10:21:50 -0500

    Carga de datos Streamlit y ETL Taxi_Zone y CO2_NYC

--- Streamlit/pages/⏫ Cargar_Datos.py
+++ Streamlit/pages/⏫ Cargar_Datos.py
@@ -5,17 +5,24 @@ import os
 import tempfile
 from google.cloud import storage
 import openpyxl
+import geopandas as gpd
+import re
 
     
-# Obtener la ruta absoluta del directorio actual y agregar el nombre del archivo de imagen
-GCP_imagen = os.path.join(os.path.dirname(__file__), "../GCP.jpeg")
-
+# Obtener la ruta absoluta del directorio actual y agregar el nombre del archivo de imagen____________________________:
+GCP_imagen = os.path.join(os.path.dirname(__file__), "../Asset/GCP.jpg")
 st.image(GCP_imagen)
-    
 
-st.markdown('### Elija un Archivo para Cargar a Google Cloud:')
+# Definir las opciones para la lista desplegable______________________________________________________________________:
 
-if st.checkbox('Emisiones de CO2 en NYC'):
+opciones = ['Emisiones de CO2 en NYC', 'Taxi_Zone']
+# Mostrar el selectbox para elegir una opción
+opcion_seleccionada = st.selectbox('Seleccione un archivo para subir a Google Cloud Platform:', opciones)
+
+
+### SUBIR DATOS DE EMISIONES DE CO2___________________________________________________________________________________:
+ 
+if opcion_seleccionada == 'Emisiones de CO2 en NYC':
     # Establecer la variable de entorno GOOGLE_APPLICATION_CREDENTIALS
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "sturdy-gate-417001-1fe4b0dcfb9d.json"
 
@@ -30,14 +37,57 @@ if st.checkbox('Emisiones de CO2 en NYC'):
         
         # Guardar el DataFrame como archivo CSV
         csv_path = os.path.splitext(file_path)[0] + ".csv"
-        df.to_csv(csv_path, index=False)
+        #df.to_csv(csv_path, index=False,sep=';')
+        df.to_csv(csv_path, index=False, sep=';', na_rep='NA', encoding='utf-8')
         
         return csv_path
 
-    # Configurar la interfaz de usuario en Streamlit
-    st.title("Cargar Emisiones de CO2 NYC:")
 
-    excel_url = st.text_input("Ingrese la URL del archivo Excel:")
+    # Función para realizar el ETL
+    def perform_etl(df):
+        # Seleccionamos las columnas que vamos a utilizar
+        columns_to_keep = ['Sectors Sector', 'Category Full', 'Category Label','Source Label','Source Units'] + [col for col in df.columns if col.startswith('CY')]
+
+        # Crear un nuevo DataFrame con las columnas seleccionadas
+        df_normalized = df[columns_to_keep].copy()
+
+        # Realizar la operación de fundido (melt)
+        df_normalized = pd.melt(df_normalized, id_vars=['Sectors Sector', 'Category Full', 'Category Label','Source Label','Source Units'], var_name='Year', value_name='Valor')
+
+        # Seleccionamos las columnas que vamos a utilizar
+        columns_to_keep = ['Sectors Sector', 'Category Full', 'Category Label','Source Label','Source Units'] + [col for col in df.columns if col.startswith('CY')]
+
+        # Crear un nuevo DataFrame con las columnas seleccionadas
+        df_normalized = df[columns_to_keep].copy()
+
+        # Realizar la operación de fundido (melt)
+        df_normalized = pd.melt(df_normalized, id_vars=['Sectors Sector', 'Category Full', 'Category Label','Source Label','Source Units'], var_name='Year', value_name='Valor')
+
+        # Crear una columna auxiliar 'Tipo' y procesar la columna 'Year'
+        df_normalized['Tipo'] = df_normalized['Year'].apply(lambda x: re.findall(r'(?:\d{4})\s(.+)', x)[0])
+        df_normalized['Year'] = df_normalized['Year'].apply(lambda x: re.findall(r'(\d{4})', x)[0])
+
+        # Filtrar solo las filas donde el tipo es 'Consumed' o 'tCO2e'
+        df_normalized = df_normalized[df_normalized['Tipo'].isin(['Consumed', 'tCO2e','Source MMBtu'])]
+
+        # Utilizar pivot_table para convertir las categorías de 'Tipo' en columnas
+        df_pivot = df_normalized.pivot_table(index=['Year','Sectors Sector', 'Category Full', 'Category Label', 'Source Label' ,'Source Units'],
+                                            columns='Tipo', values='Valor', aggfunc='sum').reset_index()
+
+        # Eliminar registros nulos
+        df_pivot.dropna(inplace=True)
+
+        # Eliminar registros duplicados
+        df_pivot.drop_duplicates(inplace=True)
+
+        return df_pivot
+
+
+    # Configurar la interfaz de usuario en Streamlit
+    #st.title("Cargar Emisiones de CO2 NYC:")
+
+    excel_url = st.text_input("Ingrese la URL del archivo en la página de MOCEJ de NYC o su ruta al drive:",value=str("https://drive.google.com/uc?id=1ZFhHtf64BgVZhzneOv1WhsyXXIX8ZyPn&export=download"))
+
     if st.button("Cargar desde URL"):
         try:
             # Realizar la solicitud para obtener el contenido del archivo Excel
@@ -52,21 +102,117 @@ if st.checkbox('Emisiones de CO2 en NYC'):
             # Convertir el archivo Excel a CSV
             temp_csv_file = convert_to_csv(temp_excel_file.name)
 
+            # Cargar el CSV en un DataFrame
+            df = pd.read_csv(temp_csv_file, sep=';')
+
+            # Realizar el ETL
+            df_transformed = perform_etl(df)
+
+            # Guardar el DataFrame transformado como un archivo CSV temporal
+            temp_csv_transformed = tempfile.NamedTemporaryFile(delete=False, suffix=".csv")
+            df_transformed.to_csv(temp_csv_transformed.name, index=False, encoding='utf-8')
+                   
             # Inicializar el cliente de Google Cloud Storage
             client_storage = storage.Client()
             bucket = client_storage.bucket(bucket_name)
 
             # Subir el archivo CSV al bucket en Google Cloud Storage
-            blob = bucket.blob(os.path.basename(temp_csv_file))
-            blob.upload_from_filename(temp_csv_file)
-
-            st.success(f"El archivo {os.path.basename(temp_csv_file)} ha sido cargado a Google Cloud Storage.")
+            blob_name ='CY_2005_CY_2022_citywide.csv'
+            blob = bucket.blob(blob_name)
+            #blob.upload_from_filename(temp_csv_file)
+            blob.upload_from_filename(temp_csv_transformed.name, content_type='text/csv')
+            
+            st.success(f"El archivo {blob_name} ha sido cargado a Google Cloud Storage.")
 
             # Eliminar archivos temporales
             os.remove(temp_excel_file.name)
             os.remove(temp_csv_file)
+ 
             
         except Exception as e:
             st.error(f"Error al cargar el archivo desde la URL: {e}")
 
-st.checkbox('Electric Car Data')
+### SUBIR DATOS DE ZONAS DE TAXI___________________________________________________________________________________:
+
+# Establecer la variable de entorno GOOGLE_APPLICATION_CREDENTIALS
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "sturdy-gate-417001-1fe4b0dcfb9d.json"
+
+# Función para cargar el archivo al bucket de Google Cloud Storage
+def upload_to_gcs(file_path, bucket_name, blob_name):
+    # Inicializar el cliente de Google Cloud Storage
+    client = storage.Client()
+
+    # Obtener el bucket
+    bucket = client.get_bucket(bucket_name)
+
+    # Subir el archivo al bucket
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(file_path)
+
+    return f"El archivo {blob_name} ha sido cargado a Google Cloud Storage."
+
+# Definir la transformación ETL para SHX
+def etl_shx(file_path):
+    # Cargar el archivo SHX usando GeoPandas
+    data = gpd.read_file(file_path)
+
+    # Definir EPSG:2263 que está en pies (LATEST WELL-KNOWN IDENTIFIER 2263)
+    data.crs = 'EPSG:2263'
+
+    # Corregir coordenadas planas a mundiales
+    data_latlon = data.to_crs('EPSG:4326')
+
+    # Calcular los centroides de los polígonos
+    data_latlon['centroid'] = data_latlon['geometry'].centroid
+
+    # Extraer las coordenadas x e y de los centroides
+    data_latlon['longitud'] = data_latlon['centroid'].x
+    data_latlon['latitud'] = data_latlon['centroid'].y
+
+    # Seleccionar las columnas deseadas y renombrarlas
+    data_latlon = data_latlon[['objectid', 'borough', 'zone', 'longitud', 'latitud']]
+    
+    # Eliminar el registro correspondiente al objectid 57
+    data_latlon = data_latlon [data_latlon ['objectid'] != "57"]
+
+    data_latlon = data_latlon [data_latlon ['objectid'] != "104"]
+    data_latlon = data_latlon [data_latlon ['objectid'] != "105"]
+
+    return data_latlon
+
+# Interfaz de usuario de Streamlit
+
+# Verificar si la opción seleccionada es "Taxi_Zone"
+if opcion_seleccionada == "Taxi_Zone":
+    # Obtener el archivo json desde el usuario
+    file = st.file_uploader("Subir archivo taxi_zone.json (fuente: https://catalog.data.gov/dataset/nyc-taxi-zones)", key="file_uploader")
+
+    # Procesar el archivo json si se ha cargado
+    if file is not None:
+        # Guardar el archivo json en el sistema temporalmente
+        with open("temp_file.json", "wb") as f:
+            f.write(file.getvalue())
+
+        # Realizar la transformación ETL
+        etl_data = etl_shx("temp_file.json")
+
+        # Guardar el resultado de la transformación como un archivo CSV
+        csv_filename = "Taxi_Zone.csv"
+        etl_data.to_csv(csv_filename, index=False)
+
+        # Nombre del archivo en el bucket
+        blob_name = csv_filename
+
+        # Nombre del bucket
+        bucket_name = "data_taxi_zone"  # Reemplaza "data_taxi_zone" por el nombre de tu bucket
+
+        # Subir el archivo CSV al bucket
+        message = upload_to_gcs(csv_filename, bucket_name, blob_name)
+
+        # Mostrar mensaje de éxito
+        st.success(message)
+
+        # Eliminar archivos temporales
+        os.remove(csv_filename )
+        os.remove("temp_file.json")
