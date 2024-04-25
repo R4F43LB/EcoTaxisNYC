b34469e89a0aa47dc8ed8905fbbb9de03edcd3c2
Alter Caimi <altercaimi@hotmail.com>
2024-04-25 13:29:42 -0300

    suprimo eliminacion de model_path

--- Streamlit/src/pages/ML_model.py
+++ Streamlit/src/pages/ML_model.py
@@ -47,7 +47,7 @@ def main():
     print('claves del diccionario: ', ensemble_sin_lstm.keys())
 
     ponderacion = ensemble_sin_lstm['ponderacion']
-    os.remove(model_path)
+    #os.remove(model_path)
 
     lstm_model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../data/lstm_model.h5'))
     lstm_model = keras.models.load_model(lstm_model_path)
