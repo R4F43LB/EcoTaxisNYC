42941818f61033494d74deaf35d0165737069a20
LDavidRojasD <ldavidrd@gmail.com>
2024-04-25 21:14:41 -0500

    Link Dashboard para embeber en Streamlit

--- Streamlit/src/pages/Dashboard.py
+++ Streamlit/src/pages/Dashboard.py
@@ -4,7 +4,7 @@ def main():
     st.title("Dashboard (Análisis de Datos):")
     
     # URL del informe de Power BI
-    power_bi_url = "https://app.powerbi.com/reportEmbed?reportId=d4d11ab5-1db8-43f8-8824-7124d0b944ee&autoAuth=true&ctid=17a02ccd-b8b8-4219-aba9-c06798c13ecd"
+    power_bi_url = "https://app.powerbi.com/reportEmbed?reportId=c3cc62f6-ac7f-4aeb-a639-8dedbdc83c1a&autoAuth=true&ctid=17a02ccd-b8b8-4219-aba9-c06798c13ecd"
     
     # Incrustar el informe de Power BI utilizando un iframe
     st.components.v1.iframe(power_bi_url, width=900, height=550)
